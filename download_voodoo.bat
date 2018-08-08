9f464da6ee40f8ec3ea6b48f7a276a52a09bffb6
canitzp <canitzp@googlemail.com>
2018-08-08 17:27:35 +0200

    download fix

--- download_voodoo.bat
+++ download_voodoo.bat
@@ -1,2 +1,2 @@
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ci.elytradev.com/job/elytra/job/Voodoo/job/rewrite/lastSuccessfulBuild/artifact/bootstrap/build/libs/bootstrap-voodoo-42.jar', 'voodoo.jar')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ci.elytradev.com/job/elytra/job/Voodoo/job/rewrite/45/artifact/bootstrap/build/libs/bootstrap-voodoo-45.jar', 'voodoo.jar')"
 PAUSE
